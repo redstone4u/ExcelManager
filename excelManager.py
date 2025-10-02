a360d3647f36fbaf7cfb22abf01f88f24e20a702
redstone4u <redstone4u@gmail.com>
2025-10-02 12:50:20 +0900

    Update excelManager.py

--- excelManager.py
+++ excelManager.py
@@ -6,7 +6,7 @@ import io
 st.set_page_config(layout="wide")
 
 # Title and description
-st.title("Excel File Merger and Converter App tested by 정민규")
+st.title("Excel File Merger and Converter App Designed By 정민규 ")
 
 # Description
 st.write(
@@ -98,4 +98,5 @@ if 'edited_df' in st.session_state:
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
         st.success(f"File saved as {file_name}")
+
 
